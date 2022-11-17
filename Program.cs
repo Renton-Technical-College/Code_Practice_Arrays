51a0981e8e5816a3ef17afee2cbd3f2c37ab5e61
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-16 18:24:12 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Practice_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        // Create 3 arrays.
+
+        // Array 1: Student name array
+
+        // Create an array that holds 5 strings
+        // Create it using the "new arrayType[capacity]" initialization
+        // Give each element a student name
+        // Ex students[2] = "Hannah";
+        // Using a for loop, display the student names
+        // Display number of students
+
+        // Example Result:
+        // Zach
+        // Wong
+        // Hannah
+        // Kris
+        // Sam
+        // 5 Students
+
+
+        // Array 2: int array
+
+        // Declare an int array
+        // Populate the array with 5 numbers on initialization
+        // Ex. { 5, 12, 8, 88, 7 }
+        // Sum all the numbers of the array into 1 variable
+        // Using a loop, display all numbers in the array and the sum
+
+        // Example Result:
+        // 5
+        // 1
+        // 2
+        // Sum: 8
+
+
+        // Array 3: Double Array - Multiply with a method
+
+        // GLOBALLY DECLARE a double array
+        // Populate it with 5 values ( your choice how )
+        // Make a method that takes one paremeter, a double[]
+
+        // In your method
+        // Loop through the array
+        // Multiply the values of the array by 5
+        // Make sure to reassign the value back to the array
+
+        // Display the doubles array before you call your method
+        // Call your method and pass your doubles array as the argument
+        // Display your doubles array after your method call
+
+        // Expected Result ( you can display how ever you like, the results are most important ):
+        // { 5.2, 7.9 , 10.0, 4, 9.7 }
+        // { 10.4, 15.8, 20.0, 8, 19.4 }
+
+
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Code_Practice_Arrays
+ Programming 120 - Code Practice - Arrays
